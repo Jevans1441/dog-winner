57d5271e01bed0752befd13d2fab836edf3ac981
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-03-01 21:10:31 -0500

    add hook to track state change

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,13 @@
 import data from "./qestiondata.json";
 import { GET_QUESTION_DATA } from "./actionTypes";
 
+// Randomizes questions before they're added ot redux store
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
-  // While there remain elements to shuffle...
   while (currentIndex !== 0) {
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
