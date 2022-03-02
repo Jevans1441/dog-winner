4c72928a98cff0673d2f6ff009d0f1dc0018e6d2
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-03-01 20:22:55 -0500

    set react router to navigate to first quesiton

--- src/views/landing.jsx
+++ src/views/landing.jsx
@@ -1,17 +1,32 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getQuestionData } from '../redux/actions'
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Question from "../components/question";
 
 const Landing = () => {
 
   const disptach = useDispatch();
+  const questions = useSelector(state => state.questions);
+  const navigateTo = useNavigate();
 
   useEffect(() => {
     disptach(getQuestionData());
   }, []);
 
+  const handleClick = () => {
+    navigateTo('q1')
+  }
+
   return (
-    <p>Landing</p>
+    <>
+      <h2>Welcome to Dog Winner</h2>
+      <p>The only game on the internet where you can win a dog for each correct answer.</p>
+      <button onClick={handleClick}>Start Game</button>
+      <Routes>
+        <Route path="/q1" element={<Question data={questions[0]} />} />
+      </Routes>
+    </>
   )
 
 };
